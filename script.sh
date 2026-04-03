29264a42c5c2a929f8d0bd72f8eb5379b8b4657c
root <root@netology.ru-central1.internal>
2026-04-03 09:08:00 +0000

    Add script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-#
+set -euo pipefail
+
+DIR="/opt/backup"
+NTW="shvirtd-example-python_backend"
+ENV_FILE="/opt/shvirtd-example-python/.env"
+DB_HOST="shvirtd-example-python-db-1"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+FILE_NAME="dumps_${TIMESTAMP}.sql"
+
+set -a
+source "$ENV_FILE"
+set +a
+
+docker exec -i "$DB_HOST" mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "
+ALTER USER '$MYSQL_USER'@'%' IDENTIFIED WITH mysql_native_password BY '$MYSQL_PASSWORD';
+FLUSH PRIVILEGES;"
+
+docker run --rm --entrypoint "" \
+  -v "$DIR:/backup" \
+  --network "$NTW" \
+  schnitzler/mysqldump \
+  mysqldump  --opt --no-tablespaces -h "$DB_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "--result-file=/backup/$FILE_NAME" "$MYSQL_DATABASE"
